2327c96c3d9578d157efc5bca662e58749e2bfa8
Tim Neil <tneil@rim.com>
2013-06-18 17:42:03 -0400

    bb10 swipe down menu update to media queries #772

--- samples/bb10/bbui.js
+++ samples/bb10/bbui.js
@@ -14,7 +14,7 @@
 * limitations under the License.
 */
 
-/* bbUI for BB10 VERSION: 0.9.6.212*/
+/* bbUI for BB10 VERSION: 0.9.6.219*/
 
 bb = {
 	scroller: null,  
@@ -1999,247 +1999,180 @@ bb.menuBar = {
 	},
 	
 	createSwipeMenu: function(menuBar, screen){
-		// Get our resolution text for BB10 styling			
-		if (bb.device.isBB10) {
-			bb.menuBar.screen = screen;
-			var bb10Menu = document.createElement('div'),
-				res = '1280x768-1280x720',
-				maxItems = 5,
-				i,
-				len,
-				type,
-				item,
-				pinLeft = false,
-				pinRight = false,
-				menuItems = [],
-				img,
-				imgPath,
-				caption,
-				div,
-				width,
-				margin,
-				bb10MenuItem;
+		bb.menuBar.screen = screen;
+		var bb10Menu = document.createElement('div'),
+			maxItems = 5,
+			i,
+			len,
+			type,
+			item,
+			pinLeft = false,
+			pinRight = false,
+			menuItems = [],
+			img,
+			imgPath,
+			caption,
+			div,
+			width,
+			margin,
+			bb10MenuItem;
 
-			// Set our 'res' for known resolutions, otherwise use the default
-			if (bb.device.is1024x600) {
-				res = '1024x600';
-				bb.menuBar.height = 100;
-				bb.menuBar.itemWidth = 96; //TODO: need to 
-			} else if (bb.device.is1280x768 || bb.device.is1280x720) {
-				res = '1280x768-1280x720';
-				bb.menuBar.height = 140;
-				bb.menuBar.itemWidth = 143;
-			} else if (bb.device.is720x720) {
-				res = '720x720';
-				bb.menuBar.height = 110;
-				bb.menuBar.itemWidth = 143;
-			}
+		// Set our 'res' for known resolutions, otherwise use the default
+		if (bb.device.is1024x600) {
+			bb.menuBar.height = 100;
+			bb.menuBar.itemWidth = 96; 
+		} else if (bb.device.is720x720) {
+			bb.menuBar.height = 110;
+			bb.menuBar.itemWidth = 143;
+		} else {
+			bb.menuBar.height = 140;
+			bb.menuBar.itemWidth = 143;
+		} 
 
-			// Handle any press-and-hold events
-			bb10Menu.oncontextmenu = function(contextEvent) {
-				var node = contextEvent.srcElement,
-					parentNode = node.parentNode;
-				// Loop up to the parent node.. if it is this action bar then prevent default
-				if (!parentNode) return;
-				while (parentNode) {
-					if (parentNode == this) {
-						contextEvent.preventDefault();
-						break;
-					}
-					parentNode = parentNode.parentNode;
-				}			
-			};
-			bb10Menu.oncontextmenu = bb10Menu.oncontextmenu.bind(bb10Menu);
-			window.addEventListener('contextmenu', bb10Menu.oncontextmenu);
-			bb.windowListeners.push({name: 'contextmenu', eventHandler: bb10Menu.oncontextmenu});
-			
-			bb10Menu.setAttribute('class','bb-bb10-menu-bar-'+res+' bb-bb10-menu-bar-dark');
-			items = menuBar.querySelectorAll('[data-bb-type=menu-item]');
-			if(items.length > 0){
-				//pre-process and collect valid menu items + identify pinned items
-				for(i = 0, len = items.length; i < items.length; i++){
-					item = items[i];
-					type = item.hasAttribute('data-bb-type') ? item.getAttribute('data-bb-type').toLowerCase() : undefined;
-					// Get our menu items
-					if (type == 'menu-item') {
-						caption = item.innerHTML;
-						imgPath = item.getAttribute('data-bb-img');
-						if (caption && imgPath) {
-							if(item.hasAttribute('data-bb-pin')){
-								pinType = item.getAttribute('data-bb-pin').toLowerCase();
-								if(pinType === 'left' && !pinLeft){
-									pinLeft = item;
-									maxItems--;
-								} else if(pinType === 'right' && !pinRight){
-									pinRight = item;
-									maxItems--;
-								} else {
-									console.log('Unknown value from menu-item data-bb-pin: ' + pinType + ' or value already defined.');
-									menuItems.push(item); //add to the regular menu array
-								}
-							} else {
-								menuItems.push(item);
-							}
-						} else {
-							console.log('missing menu item caption or image.');
-						}
-					} else {
-						console.log('invalid menu item type for bb10');
-					}
+		// Handle any press-and-hold events
+		bb10Menu.oncontextmenu = function(contextEvent) {
+			var node = contextEvent.srcElement,
+				parentNode = node.parentNode;
+			// Loop up to the parent node.. if it is this action bar then prevent default
+			if (!parentNode) return;
+			while (parentNode) {
+				if (parentNode == this) {
+					contextEvent.preventDefault();
+					break;
 				}
-
-				//trim down items if too many
-				if(menuItems.length >= maxItems){
-					menuItems = menuItems.slice(0, maxItems);
-				}
-
-				//add back left and right pinned items if they exist
-				if(pinLeft){
-					menuItems.unshift(pinLeft);
-				}
-
-				if(pinRight){
-					menuItems.push(pinRight);
-				}
-
-				width = bb.menuBar.itemWidth + 'px';
-				margin = Math.floor((window.innerWidth - (bb.menuBar.itemWidth  * menuItems.length)) / (menuItems.length-1)) + 'px';
-				for (i = 0, len = menuItems.length; i < len; i++) {
-					item = menuItems[i];
+				parentNode = parentNode.parentNode;
+			}			
+		};
+		bb10Menu.oncontextmenu = bb10Menu.oncontextmenu.bind(bb10Menu);
+		window.addEventListener('contextmenu', bb10Menu.oncontextmenu);
+		bb.windowListeners.push({name: 'contextmenu', eventHandler: bb10Menu.oncontextmenu});
+		
+		bb10Menu.setAttribute('class','bb-menu-bar bb-menu-bar-dark');
+		items = menuBar.querySelectorAll('[data-bb-type=menu-item]');
+		if(items.length > 0){
+			//pre-process and collect valid menu items + identify pinned items
+			for(i = 0, len = items.length; i < items.length; i++){
+				item = items[i];
+				type = item.hasAttribute('data-bb-type') ? item.getAttribute('data-bb-type').toLowerCase() : undefined;
+				// Get our menu items
+				if (type == 'menu-item') {
 					caption = item.innerHTML;
 					imgPath = item.getAttribute('data-bb-img');
-
-					bb10MenuItem = document.createElement("div");
-					// Set our item information
-					bb10MenuItem.setAttribute('class','bb-bb10-menu-bar-item-'+res);
-					item.innerHTML = '';
-					// Add the image
-					img = document.createElement('img');
-					img.setAttribute('src',imgPath);
-					bb10MenuItem.appendChild(img);
-					// Add the caption
-					div = document.createElement('div');
-					div.setAttribute('class','bb-bb10-menu-bar-item-caption-'+res);
-					div.innerHTML = caption;
-					bb10MenuItem.appendChild(div);
-
-					// Assign any click handlers
-					bb10MenuItem.onclick	= item.onclick;
-					//set menu item width
-					bb10MenuItem.style.width = width;
-					if ((i == menuItems.length - 1 && menuItems.length > 1 ) || (menuItems.length === 1 && !pinLeft))  {
-	                    bb10MenuItem.style.marginRight = 0;
-						bb10MenuItem.style.float = 'right';
-					} else {
-	                    bb10MenuItem.style.marginRight = margin;
-					}
-					bb10Menu.appendChild(bb10MenuItem);
-
-					bb10MenuItem.ontouchstart = function() {
-						this.style['border-top-color'] = bb.options.highlightColor;
-					}
-					bb10MenuItem.ontouchend = function() {
-						this.style['border-top-color'] = 'transparent';
-					}
-				}
-			} else {
-				bb10Menu.style.display = 'none';
-				bb.menuBar.menu = null;
-			}
-
-			// Set the size of the menu bar and assign the lstener
-			bb10Menu.addEventListener('click', bb.menuBar.onMenuBarClicked, false);
-			screen.parentNode.appendChild(bb10Menu);
-			// Assign the menu
-			bb.menuBar.menu	= bb10Menu;	
-			bb.menuBar.menu.style['z-index'] = '-100';
-			bb.menuBar.menu.style.display = 'none';
-			bb.menuBar.menu.style.height = bb.menuBar.menu.height + 'px';
-
-			bb.menuBar.menu.doOrientationChange = function() {
-				var i, len,
-					menuItems = bb.menuBar.menu.getElementsByClassName('bb-bb10-menu-bar-item-'+res),
-					margin = Math.floor((window.innerWidth - (bb.menuBar.itemWidth * menuItems.length)) / (menuItems.length-1)) + 'px';
-				for(i = 0, len = menuItems.length; i < len; i++){
-					if (i == menuItems.length - 1) {
-	                    menuItems[i].style.marginRight = 0;
-						menuItems[i].style.float = 'right';
-					} else {
-	                    menuItems[i].style.marginRight = margin;
-					}
-				}
-			};
-			
-			bb.menuBar.menu.doOrientationChange = bb.menuBar.menu.doOrientationChange.bind(bb.menuBar);
-			window.addEventListener('resize', bb.menuBar.menu.doOrientationChange,false); 
-			bb.windowListeners.push({name: 'resize', eventHandler: bb.menuBar.menu.doOrientationChange});
-		} else {
-			var pbMenu = document.createElement('div'), 
-				items, 
-				pbMenuInner, 
-				j,
-				item,
-				img, 
-				title, 
-				div, 
-				br, 
-				pbMenuItem;
-			bb.menuBar.height = 100;
-			pbMenu.setAttribute('class','pb-menu-bar');
-			// See if there are any items declared
-			items = menuBar.getElementsByTagName('div');
-			if(items.length > 0){
-				pbMenuInner	= document.createElement("ul");
-				pbMenu.appendChild(pbMenuInner);				
-				// Loop through our menu items
-				for (j = 0; j < items.length; j++) {
-					item = items[j];
-					if(item.getAttribute('data-bb-type') === "menu-item"){
-						// Assign our values
-						title = item.innerHTML
-						iconPath = item.getAttribute('data-bb-img');
-
-						// If they don't hav both an icon and a title ignore the item
-						if (title && iconPath) {
-							// Create our item
-							pbMenuItem = document.createElement("li");
-							item.innerHTML = '';
-							
-							// Get our image
-							img	= new Image();
-							img.src	= iconPath;
-							pbMenuItem.appendChild(img);
-								
-							// Add our caption
-							div = document.createElement('div');
-							div.setAttribute('class','pb-menu-bar-caption');
-							div.innerText = title;
-							pbMenuItem.appendChild(div);
-							
-							// Assign any click handlers
-							pbMenuItem.onclick	= item.onclick;
-							pbMenuInner.appendChild(pbMenuItem);
+					if (caption && imgPath) {
+						if(item.hasAttribute('data-bb-pin')){
+							pinType = item.getAttribute('data-bb-pin').toLowerCase();
+							if(pinType === 'left' && !pinLeft){
+								pinLeft = item;
+								maxItems--;
+							} else if(pinType === 'right' && !pinRight){
+								pinRight = item;
+								maxItems--;
+							} else {
+								console.log('Unknown value from menu-item data-bb-pin: ' + pinType + ' or value already defined.');
+								menuItems.push(item); //add to the regular menu array
+							}
+						} else {
+							menuItems.push(item);
 						}
-					} else if(item.getAttribute('data-bb-type') === "menu-separator"){
-						pbMenuInner	= document.createElement('ul');
-						pbMenu.appendChild(pbMenuInner);
-					} else{
-						console.log('invalid menu item type');
+					} else {
+						console.log('missing menu item caption or image.');
 					}
+				} else {
+					console.log('invalid menu item type for bb10');
 				}
 			}
-			// Set the size of the menu bar and assign the lstener
-			pbMenu.style['-webkit-transform']	= 'translate(0,0)';
-			pbMenu.addEventListener('click', bb.menuBar.onMenuBarClicked, false);
-			document.body.appendChild(pbMenu);
-			// Assign the menu
-			bb.menuBar.menu	= pbMenu;	
+
+			//trim down items if too many
+			if(menuItems.length >= maxItems){
+				menuItems = menuItems.slice(0, maxItems);
+			}
+
+			//add back left and right pinned items if they exist
+			if(pinLeft){
+				menuItems.unshift(pinLeft);
+			}
+
+			if(pinRight){
+				menuItems.push(pinRight);
+			}
+
+			width = bb.menuBar.itemWidth + 'px';
+			margin = Math.floor((window.innerWidth - (bb.menuBar.itemWidth  * menuItems.length)) / (menuItems.length-1)) + 'px';
+			for (i = 0, len = menuItems.length; i < len; i++) {
+				item = menuItems[i];
+				caption = item.innerHTML;
+				imgPath = item.getAttribute('data-bb-img');
+
+				bb10MenuItem = document.createElement("div");
+				// Set our item information
+				bb10MenuItem.setAttribute('class','bb-menu-bar-item');
+				item.innerHTML = '';
+				// Add the image
+				img = document.createElement('img');
+				img.setAttribute('src',imgPath);
+				bb10MenuItem.appendChild(img);
+				// Add the caption
+				div = document.createElement('div');
+				div.setAttribute('class','bb-menu-bar-item-caption');
+				div.innerHTML = caption;
+				bb10MenuItem.appendChild(div);
+
+				// Assign any click handlers
+				bb10MenuItem.onclick	= item.onclick;
+				//set menu item width
+				bb10MenuItem.style.width = width;
+				if ((i == menuItems.length - 1 && menuItems.length > 1 ) || (menuItems.length === 1 && !pinLeft))  {
+					bb10MenuItem.style.marginRight = 0;
+					bb10MenuItem.style.float = 'right';
+				} else {
+					bb10MenuItem.style.marginRight = margin;
+				}
+				bb10Menu.appendChild(bb10MenuItem);
+
+				bb10MenuItem.ontouchstart = function() {
+					this.style['border-top-color'] = bb.options.highlightColor;
+				}
+				bb10MenuItem.ontouchend = function() {
+					this.style['border-top-color'] = 'transparent';
+				}
+			}
+		} else {
+			bb10Menu.style.display = 'none';
+			bb.menuBar.menu = null;
 		}
+
+		// Set the size of the menu bar and assign the lstener
+		bb10Menu.addEventListener('click', bb.menuBar.onMenuBarClicked, false);
+		screen.parentNode.appendChild(bb10Menu);
+		// Assign the menu
+		bb.menuBar.menu	= bb10Menu;	
+		bb.menuBar.menu.style['z-index'] = '-100';
+		bb.menuBar.menu.style.display = 'none';
+		bb.menuBar.menu.style.height = bb.menuBar.menu.height + 'px';
+
+		bb.menuBar.menu.doOrientationChange = function() {
+			var i, len,
+				menuItems = bb.menuBar.menu.getElementsByClassName('bb-menu-bar-item'),
+				margin = Math.floor((window.innerWidth - (bb.menuBar.itemWidth * menuItems.length)) / (menuItems.length-1)) + 'px';
+			for(i = 0, len = menuItems.length; i < len; i++){
+				if (i == menuItems.length - 1) {
+					menuItems[i].style.marginRight = 0;
+					menuItems[i].style.float = 'right';
+				} else {
+					menuItems[i].style.marginRight = margin;
+				}
+			}
+		};
+		
+		bb.menuBar.menu.doOrientationChange = bb.menuBar.menu.doOrientationChange.bind(bb.menuBar);
+		window.addEventListener('resize', bb.menuBar.menu.doOrientationChange,false); 
+		bb.windowListeners.push({name: 'resize', eventHandler: bb.menuBar.menu.doOrientationChange});
+	
 		
 		// Add the overlay for trapping clicks on items below
 		if (!bb.screen.overlay) {
 			bb.screen.overlay = document.createElement('div');
-			bb.screen.overlay.setAttribute('class','bb-bb10-menu-bar-overlay');
+			bb.screen.overlay.setAttribute('class','bb-menu-bar-overlay');
 		}
 		screen.appendChild(bb.screen.overlay);
 		bb.menuBar.menu.overlay = bb.screen.overlay;	
