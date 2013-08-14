c4cbf624fb074274ed866c5a20da8c4ece77e3ca
Tim Neil <tneil@rim.com>
2013-08-14 09:05:48 -0400

    fix for mistaken color in action bar issue #953

--- pkg/bb10/bbui.js
+++ pkg/bb10/bbui.js
@@ -14,7 +14,7 @@
 * limitations under the License.
 */
 
-/* bbUI for BB10 VERSION: 0.9.6.688*/
+/* bbUI for BB10 VERSION: 0.9.6.689*/
 
 bb = {
 	scroller: null,  
--- pkg/bb10/ext/index.js
+++ pkg/bb10/ext/index.js
@@ -14,7 +14,7 @@
 * limitations under the License.
 */
 
-/* bbUI for BB10 VERSION: 0.9.6.688*/
+/* bbUI for BB10 VERSION: 0.9.6.689*/
 
 var LIB_FOLDER = '../../lib/',
 	_bbUI_overlayWebView,
