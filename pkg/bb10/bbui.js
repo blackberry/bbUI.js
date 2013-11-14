02e6f922d41f7c207799cefd542bc2562e660eef
Tim Neil <tneil@rim.com>
2013-11-14 12:02:24 -0500

    fix for action bar issue #989

--- pkg/bb10/bbui.js
+++ pkg/bb10/bbui.js
@@ -14,7 +14,7 @@
 * limitations under the License.
 */
 
-/* bbUI for BB10 VERSION: 0.9.6.904*/
+/* bbUI for BB10 VERSION: 0.9.6.920*/
 
 bb = {
 	scroller: null,  
--- pkg/bb10/ext/client.js
+++ pkg/bb10/ext/client.js
@@ -14,7 +14,7 @@
 * limitations under the License.
 */
 
-/* bbUI for BB10 VERSION: 0.9.6.904*/
+/* bbUI for BB10 VERSION: 0.9.6.920*/
 
 var _self = {},
     _ID = require("./manifest.json").namespace;
