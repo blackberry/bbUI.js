96201df31d44e2725aafdb7e12ea158a426ed29a
Tim Neil <tneil@rim.com>
2014-05-12 10:36:57 -0400

    updates for issue #996

---
+++ samples/bb10/platforms/blackberry10/.staging/www/plugins/com.blackberry.bbui/www/client.js
@@ -0,0 +1,31 @@
+cordova.define("com.blackberry.bbui.client", function(require, exports, module) { /*
+* Copyright 2010-2012 Research In Motion Limited.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+/* VERSION: 0.9.6.130*/
+
+var _self = {},
+    exec = cordova.require("cordova/exec"),
+    noop = function () {},
+    _ID = "com.blackberry.bbui";
+
+_self.initContext = function (value) {
+    exec(noop, noop, _ID, "initContext", {value : value}, false);
+}
+
+module.exports = _self;
+
+
+});
--- samples/bb10/platforms/blackberry10/www/cordova_plugins.js
+++ samples/bb10/platforms/blackberry10/www/cordova_plugins.js
@@ -6,12 +6,28 @@ module.exports = [
         "clobbers": [
             "blackberry.app"
         ]
+    },
+    {
+        "file": "plugins/com.blackberry.bbui/www/client.js",
+        "id": "com.blackberry.bbui.client",
+        "clobbers": [
+            "blackberry.bbui"
+        ]
+    },
+    {
+        "file": "plugins/com.blackberry.ui.contextmenu/www/client.js",
+        "id": "com.blackberry.ui.contextmenu.client",
+        "clobbers": [
+            "blackberry.ui.contextmenu"
+        ]
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "com.blackberry.app": "1.0.0"
+    "com.blackberry.app": "1.0.0",
+    "com.blackberry.bbui": "1.0.0",
+    "com.blackberry.ui.contextmenu": "1.0.0"
 }
 // BOTTOM OF METADATA
 });
