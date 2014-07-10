6491a6b4d5b0ce095a3f7a175a01de4bfbe41ace
Tim Neil <tneil@rim.com>
2014-07-10 12:36:13 -0400

    slider update for passport for issue #1018

--- pkg/bb10/bbui.js
+++ pkg/bb10/bbui.js
@@ -14,7 +14,7 @@
 * limitations under the License.
 */
 
-/* bbUI for BB10 VERSION: 0.9.6.1728*/
+/* bbUI for BB10 VERSION: 0.9.6.1732*/
 
 bb = {
 	scroller: null,  
--- pkg/bb10/ext/client.js
+++ pkg/bb10/ext/client.js
@@ -14,7 +14,7 @@
 * limitations under the License.
 */
 
-/* bbUI for BB10 VERSION: 0.9.6.1728*/
+/* bbUI for BB10 VERSION: 0.9.6.1732*/
 
 var _self = {},
     _ID = require("./manifest.json").namespace;
